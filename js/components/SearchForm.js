0d04cd39cd0077564cfa42f0990790e1957904e8
wsty-gif <daiki1998321@gmail.com>
2025-10-28 18:50:49 +0900

    Revert "Reapply "コミット""
    This reverts commit 8674eccc10eeef34b0b13b103b688d564d6b6366.

--- js/components/SearchForm.js
+++ js/components/SearchForm.js
@@ -354,6 +354,7 @@ class SearchForm {
     document.getElementById("apply-loc").onclick=()=>{
       this.state.locations = Array.from(this._tempLoc);
       this.updateConditionLabels();
+      this.render(); 
       this.closeSlide("loc");
     };
   }
@@ -597,6 +598,7 @@ class SearchForm {
       const checked=Array.from(document.querySelectorAll("#page-job .job-chk:checked")).map(c=>c.value);
       this.state.jobs=checked;
       this.updateConditionLabels();
+      this.render();
       this.closeSlide("job");
     };
 
@@ -671,6 +673,7 @@ class SearchForm {
       const checked=Array.from(document.querySelectorAll("#page-pref .pref-chk:checked")).map(c=>c.value);
       this.state.prefs=checked;
       this.updateConditionLabels();
+      this.render();
       this.closeSlide("pref");
     };
   }
